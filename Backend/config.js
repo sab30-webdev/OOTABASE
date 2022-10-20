a4b6574559675e0eedc37646a76a6252f5ffd7e0
Nuthan Hegde <nuthan@nullish.tech>
2022-10-20 21:30:55 +0530

    Setup environment vars to connect to azure db

--- Backend/config.js
+++ Backend/config.js
@@ -1,8 +1,8 @@
 const config = {
-  user: "sab",
-  password: "qwerty",
-  server: "localhost",
-  database: "RESTAURANT",
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB,
   synchronize: true,
   trustServerCertificate: true,
 };
