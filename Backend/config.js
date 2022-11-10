7e741efa7e764121d2748e55671242bcea0ec3a9
Sabarinath <62168230+sab30-webdev@users.noreply.github.com>
2022-11-10 23:44:33 +0530

    addded .env

--- Backend/config.js
+++ Backend/config.js
@@ -1,28 +1,10 @@
-// const config = {
-//   user: "sab",
-//   password: "qwerty",
-//   server: "localhost",
-//   database: "RESTAURANT",
-//   synchronize: true,
-//   trustServerCertificate: true,
-// };
-
 const config = {
-  user: "sab",
-  password: "qwerty",
-  server: "localhost",
-  database: "oota",
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB,
   synchronize: true,
   trustServerCertificate: true,
 };
-
-// const config = {
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PWD,
-//   server: process.env.DB_SERVER,
-//   database: process.env.DB,
-//   synchronize: true,
-//   trustServerCertificate: true,
-// };
 
 module.exports = config;
